450ab5caf3da87cfe70a83613f376586f07f445f
Ole Jørgen Skogstad <skogstad@softis.net>
2022-12-02 22:51:55 +0100

    First

---
+++ src/01/Program.cs
@@ -0,0 +1,19 @@
+﻿var input = File.ReadAllLines("./calories_input");
+
+var caloriesPerElf = new List<int>();
+var calorieCountCurrentElf = 0;
+
+foreach (var line in input)
+{
+    if (string.IsNullOrWhiteSpace(line))
+    {
+       caloriesPerElf.Add(calorieCountCurrentElf);
+       calorieCountCurrentElf = 0;
+       continue;
+    }
+
+    var num = int.Parse(line);
+    calorieCountCurrentElf += num;
+}
+
+Console.WriteLine(caloriesPerElf.OrderDescending().First());
