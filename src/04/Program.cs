97e178c12f0075dbed1c306546b931418f618d1e
Ole Jørgen Skogstad <skogstad@softis.net>
2022-12-04 09:09:01 +0100

    04 Part two

--- src/04/Program.cs
+++ src/04/Program.cs
@@ -1,14 +1,28 @@
 ﻿using System.Text.RegularExpressions;
 
-var sectionAssignments = File.ReadAllLines("./section_assignments");
+var sectionAssignmentsInput = File.ReadAllLines("./section_assignments");
 
-var numberOfTotalOverlaps = sectionAssignments
-    .Select(sectionAssignment => 
+var sectionAssignments = sectionAssignmentsInput
+    .Select(sectionAssignment =>
         Regex.Split(sectionAssignment, @"\D+")
             .Select(int.Parse)
             .ToList())
-    .Count(sections => 
-        (sections[0] >= sections[2] && sections[1] <= sections[3]) || 
-        (sections[0] <= sections[2] && sections[1] >= sections[3]));
+    .ToList();
 
+bool HasTotalOverLap(IReadOnlyList<int> sections) =>
+    (sections[0] >= sections[2] && sections[1] <= sections[3]) ||
+    (sections[0] <= sections[2] && sections[1] >= sections[3]);
+
+var numberOfTotalOverlaps = sectionAssignments.Count(HasTotalOverLap);
 Console.WriteLine($"Number of total overlaps: {numberOfTotalOverlaps}");
+
+bool IsInRange(int num, int start, int end) => num >= start && num <= end;
+
+bool HasPartialOverLap(IReadOnlyList<int> sections) =>
+    IsInRange(sections[0], sections[2], sections[3]) ||
+    IsInRange(sections[1], sections[2], sections[3]) ||
+    IsInRange(sections[2], sections[0], sections[1]) ||
+    IsInRange(sections[3], sections[0], sections[1]);
+
+var numberOfOverlaps = sectionAssignments.Count(HasPartialOverLap);
+Console.WriteLine($"Number of overlaps: {numberOfOverlaps}");
