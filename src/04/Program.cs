09560f096f0f784ee7e06ebce39f6cd89a22bc4b
Ole Jørgen Skogstad <skogstad@softis.net>
2022-12-04 08:13:39 +0100

    04 Part one

---
+++ src/04/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.RegularExpressions;
+
+var sectionAssignments = File.ReadAllLines("./section_assignments");
+
+var numberOfTotalOverlaps = sectionAssignments
+    .Select(sectionAssignment => 
+        Regex.Split(sectionAssignment, @"\D+")
+            .Select(int.Parse)
+            .ToList())
+    .Count(sections => 
+        (sections[0] >= sections[2] && sections[1] <= sections[3]) || 
+        (sections[0] <= sections[2] && sections[1] >= sections[3]));
+
+Console.WriteLine($"Number of total overlaps: {numberOfTotalOverlaps}");
