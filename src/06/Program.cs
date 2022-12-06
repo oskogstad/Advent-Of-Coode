e6ca288faf47905cb04d108712fd741bd2ab91fc
Ole Jørgen Skogstad <skogstad@softis.net>
2022-12-06 14:30:40 +0100

    06 Part one

---
+++ src/06/Program.cs
@@ -0,0 +1,35 @@
+﻿var signal = new Span<char>(File.ReadAllText("./signal").ToCharArray());
+
+var markerFound = false;
+
+var index = 0;
+const int markerSize = 4;
+
+bool SpanContainsUniqueCharacters(Span<char> span)
+{
+   var checkedCharacters = new List<char>();
+   foreach (var character in span)
+   {
+      if (checkedCharacters.Contains(character))
+         return false;
+      
+      checkedCharacters.Add(character);
+   }
+
+   return true;
+}
+
+while (!markerFound)
+{
+   var currentSpan = signal.Slice(index, markerSize);
+
+   if (SpanContainsUniqueCharacters(currentSpan))
+   {
+      markerFound = true;
+      continue;
+   }
+
+   index++;
+}
+
+Console.WriteLine($"06 Part one: {index+4}");
