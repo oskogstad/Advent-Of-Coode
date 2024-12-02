decb3adae7e608abd7b4706bad5588bdf0c030ae
Ole Jørgen Skogstad <skogstad@softis.net>
2024-12-02 19:29:09 +0100

    2024 01 part 1

---
+++ 01/Program.cs
@@ -0,0 +1,18 @@
+﻿var distanceLists = File.ReadAllLines("./distance_lists").ToList();
+
+var leftList = distanceLists
+    .Select(distance => int.Parse(distance.Split("   ")[0]))
+    .OrderBy(x => x)
+    .ToList();
+
+var rightList = distanceLists
+    .Select(distance => int.Parse(distance.Split("   ")[1]))
+    .OrderBy(x => x)
+    .ToList();
+
+var sum = leftList
+    .Zip(rightList, (left, right)
+        => Math.Abs(left - right))
+    .Sum();
+
+Console.WriteLine($"Sum: {sum}");
